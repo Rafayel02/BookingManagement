bfc8eee3a20e56f5bb809aa423fc4c0603ca8189
tigranha <tik.hakobyan08@gmail.com>
2021-10-10 19:52:57 +0400

    category entity

---
+++ src/main/java/am/aca/bookingmanagement/model/Category.java
@@ -0,0 +1,21 @@
+package am.aca.bookingmanagement.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
