fe4caa974af6286b92ccb9159192fe07578070ae
MaCodeAm <mariam.ghazaryan96@yahoo.com>
2021-10-13 20:17:57 +0400

    DDl, entity-classes

--- src/main/java/am/aca/bookingmanagement/model/Category.java
+++ src/main/java/am/aca/bookingmanagement/model/Category.java
@@ -4,18 +4,27 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Table(name = "categories")
 @Getter
 @Setter
-public class Category {
+public class Category implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "restaurants_categories",
+            joinColumns = {@JoinColumn(name = "type_id")},
+            inverseJoinColumns = {@JoinColumn(name = "restaurant_id")})
+    private List<Restaurant> restaurants;
 
 }
--- src/main/java/am/aca/bookingmanagement/model/Restaurant.java
+++ src/main/java/am/aca/bookingmanagement/model/Restaurant.java
@@ -2,16 +2,17 @@ package am.aca.bookingmanagement.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
-import java.util.Map;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
 
 @Entity
 @Table(name = "restaurants")
 @Getter
 @Setter
-public class Restaurant {
+public class Restaurant implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,11 +21,24 @@ public class Restaurant {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "location")
-    @Type(type = "jsonb")
-    private Map<String, Double> location;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
 
-    @Column(name = "address")
+    @Column(name = "longitude", nullable = false)
+    private double longitude;
+
+    @Column(name = "latitude", nullable = false)
+    private double latitude;
+
+    @Column(name = "address", nullable = false)
     private String address;
 
+    @Column(name = "image", nullable = true)
+    private String image;
+
+    @ManyToMany(mappedBy = "restaurants")
+    private List<Category> categories;
+
+    @OneToMany(mappedBy = "restaurants")
+    private List<Reviews> reviews;
 }
