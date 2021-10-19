08114e02101c8635014a664e48dfcda5035314a3
tigranha <tik.hakobyan08@gmail.com>
2021-10-19 16:26:02 +0400

    partner password encoder added

--- src/main/java/am/aca/bookingmanagement/BookingManagement.java
+++ src/main/java/am/aca/bookingmanagement/BookingManagement.java
@@ -2,8 +2,10 @@ package am.aca.bookingmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:beans/passwordEncoder.xml")
 public class BookingManagement {
 
     public static void main(String[] args) {
