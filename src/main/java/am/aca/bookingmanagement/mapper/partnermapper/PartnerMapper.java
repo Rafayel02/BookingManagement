dae6e9fc31b33124fe3140012df0c6056feeb4c7
unknown <rafayel.shahnazaryan@gmail.com>
2021-10-19 09:15:45 +0400

    packaging

--- src/main/java/am/aca/bookingmanagement/mapper/partnermapper/PartnerMapper.java
+++ src/main/java/am/aca/bookingmanagement/mapper/partnermapper/PartnerMapper.java
@@ -1,8 +1,8 @@
 package am.aca.bookingmanagement.mapper.partnermapper;
 
-import am.aca.bookingmanagement.dto.partnerdto.PartnerLoginResponseDetails;
-import am.aca.bookingmanagement.dto.partnerdto.PartnerRegisterRequestDetails;
-import am.aca.bookingmanagement.dto.partnerdto.PartnerRegisterResponseDetails;
+import am.aca.bookingmanagement.dto.partnerdto.login.PartnerLoginResponseDetails;
+import am.aca.bookingmanagement.dto.partnerdto.register.PartnerRegisterRequestDetails;
+import am.aca.bookingmanagement.dto.partnerdto.register.PartnerRegisterResponseDetails;
 import am.aca.bookingmanagement.entity.Partner;
 
 public interface PartnerMapper {
