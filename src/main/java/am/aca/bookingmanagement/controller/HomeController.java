78953565e71b495885c9dac3aadb8ea1dbcad9e4
unknown <rafayel.shahnazaryan@gmail.com>
2021-10-21 10:37:12 +0400

    getting token from header

--- src/main/java/am/aca/bookingmanagement/controller/HomeController.java
+++ src/main/java/am/aca/bookingmanagement/controller/HomeController.java
@@ -4,10 +4,11 @@ import am.aca.bookingmanagement.jwt.JwtTokenParser;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/")
@@ -19,12 +20,20 @@ public class HomeController {
         this.jwtTokenParser = jwtTokenParser;
     }
 
-    @PostMapping
-    public ResponseEntity<?> home(@RequestParam final String token) {
-        final Jws<Claims> claims = jwtTokenParser.parse(token);
+    @GetMapping
+    public ResponseEntity<?> getBearerTokenHeader() {
+        String authorizationToken =
+                ((ServletRequestAttributes) Objects
+                        .requireNonNull(
+                                RequestContextHolder
+                                        .getRequestAttributes()))
+                        .getRequest()
+                        .getHeader("Authorization")
+                        .replace("Bearer ", "");
+        final Jws<Claims> claims = jwtTokenParser.parse(authorizationToken);
         final Claims body = claims.getBody();
         final String uuid = (String) body.get("id");
-        //TODO check if {uuid} id present in users or partners table and response accordingly
+        System.out.println(uuid);
         return ResponseEntity.ok(uuid);
     }
 
