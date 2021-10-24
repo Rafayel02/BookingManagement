94300af5b74111cc5ca943029524a29609325067
MaCodeAm <mariam.ghazaryan96@yahoo.com>
2021-10-24 14:24:44 +0400

    ADD: Missing Methods in CategoriRpeository

--- src/main/java/am/aca/bookingmanagement/repository/CategoryRepository.java
+++ src/main/java/am/aca/bookingmanagement/repository/CategoryRepository.java
@@ -2,6 +2,7 @@ package am.aca.bookingmanagement.repository;
 
 import am.aca.bookingmanagement.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     Optional<Category> findByType(String type);
 
+    @Query(value = "select id from categories where type = ?1", nativeQuery = true)
+    Optional<Integer> findCategoryIdByType(String type);
 }
