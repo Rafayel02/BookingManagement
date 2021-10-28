38bcaec8d4c16ee4fe3a0f2b3bb5285b70597021
MaCodeAm <mariam.ghazaryan96@yahoo.com>
2021-10-28 16:31:32 +0400

    filter bug fixed (back)

--- src/main/java/am/aca/bookingmanagement/repository/FilterRepository.java
+++ src/main/java/am/aca/bookingmanagement/repository/FilterRepository.java
@@ -22,11 +22,11 @@ public interface FilterRepository extends JpaRepository<Partner, Long>, JpaSpeci
 
     @Query(value = "select distinct * " +
             "from (select distinct * " +
-            "      from partners p inner join (select * from partners_categories pc where pc.type_id in ?1) pc on p.id = pc.partner_id) pm " +
-            "         inner join (select * from partners_activities pa where pa.type_id in ?2) pa on pa.partner_id = pm.id;", nativeQuery = true)
+            "      from partners p inner join (select distinct partner_id from partners_categories pc where pc.type_id in (:listCat)) pc on p.id = pc.partner_id) pm " +
+            "         inner join (select distinct partner_id from partners_activities pa where pa.type_id in (:listAct)) pa on pa.partner_id = pm.id;", nativeQuery = true)
     List<Partner> findByCategoryAndActivity(@Param("listCat") List<Integer> listCat,
-                                           @Param("listAct") List<Integer> listAct);
+                                            @Param("listAct") List<Integer> listAct);
 
-    @Query(value = "select distinct * from partners where partners.id = :list", nativeQuery = true)
+    @Query(value = "select * from partners where partners.id in :list", nativeQuery = true)
     List<Partner> findById(@Param("list") List<Integer> partnerSIdList);
 }
